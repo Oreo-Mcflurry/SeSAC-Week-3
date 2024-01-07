9032f92f398076880f7389c077c47c6adbb34b60
유인호 <yuiop1029@naver.com>
2024-01-07 21:02:31 +0900

    버튼 체크까진 되는데, 다른걸 추가하면 다시 원상복귀 되어버림.

---
+++ SeSAC Week 3/daily 9/Case2/Case2TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  Case2TableViewCell.swift
+//  SeSAC Week 3
+//
+//  Created by A_Mcflurry on 1/6/24.
+//
+
+import UIKit
+
+class Case2TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SeSAC Week 3/daily 9/Case3/Case3TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  Case3TableViewCell.swift
+//  SeSAC Week 3
+//
+//  Created by A_Mcflurry on 1/7/24.
+//
+
+import UIKit
+
+class Case3TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
